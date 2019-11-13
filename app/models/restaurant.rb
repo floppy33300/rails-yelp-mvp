8840ef6c7e507dfc94cb2bbf78e91bc5660e2d11
floppy dublao <floriandbc@gmail.com>
2019-11-13 16:21:16 +0100

    exo2

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  validates :category, inclusion: { in: CATEGORIES }
+  has_many :reviews, dependent: :destroy
+end
